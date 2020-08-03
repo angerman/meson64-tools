d47bbf32434fd6816be7f09670c8d34fee72b82e
Moritz Angermann <moritz.angermann@gmail.com>
2020-08-03 16:20:40 +0800

    Fix python2 *and* python3 dependency

--- default.nix
+++ default.nix
@@ -1,8 +1,17 @@
 { nixpkgs ? import <nixpkgs> {} }:
 {
     meson64-tools = nixpkgs.stdenv.mkDerivation {
+
+        nativeBuildInputs = [ nixpkgs.python2 nixpkgs.python3 ];
+
         name = "meson64-tools";
         src = ./.;
+
+        preBuild = ''
+            patchShebangs .
+            patchShebangs ./mbedtls/scripts/generate_psa_constants.py
+        '';
+
         makeFlags = [ "PREFIX=$(out)" ];
     };
 }
